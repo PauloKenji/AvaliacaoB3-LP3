9eb7b738eff0ecc64d131869ac4f25a63786a903
Paulo Kenji <paulomuneischi@gmail.com>
2022-09-18 23:35:33 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Avaliacao3b_LP3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
